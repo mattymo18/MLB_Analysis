b3acb878ecfe0eda36e1a6524ff1ea9c2e505da8
MATT JOHNSON <johnson.matt1818@gmail.com>
2021-04-27 20:21:29 -0400

    Only makes ppt images

--- K-Means_Cluster.R
+++ K-Means_Cluster.R
@@ -30,7 +30,7 @@ km.optim.clust <- function(DF) {
   DF.cent = na.omit(cbind(cent.temp, 
                           scale(DF %>% 
                                   select(-c(AllStar, playerID, yearID, 
-                                            stint, teamID, lgID, POS)), scale = T, center = T)))
+                                            teamID, lgID, POS)), scale = T, center = T)))
   #check number of clusters
   
   fviz_nbclust(DF.cent[, -1], kmeans, method = "wss") +
@@ -49,7 +49,7 @@ km.clust <- function(DF) {
   DF.cent = na.omit(cbind(cent.temp, 
                           scale(DF %>% 
                                   select(-c(AllStar, playerID, yearID, 
-                                            stint, teamID, lgID, POS)), scale = T, center = T)))
+                                            teamID, lgID, POS)), scale = T, center = T)))
 
   k = kmeans(DF.cent[, -1], centers = 2, nstart = 10)
   
@@ -70,40 +70,42 @@ km.clust <- function(DF) {
 
 #we can check them all for the right number of clusters (hopefully 2)
 
-p1 <- km.optim.clust(B1)
-p2 <- km.optim.clust(B2)
-p3 <- km.optim.clust(B3)
+# p1 <- km.optim.clust(B1)
+# p2 <- km.optim.clust(B2)
+# p3 <- km.optim.clust(B3)
 p4 <- km.optim.clust(SS)
-p5 <- km.optim.clust(OF)
-p6 <- km.optim.clust(Catchers)
+# p5 <- km.optim.clust(OF)
+# p6 <- km.optim.clust(Catchers)
 
 #now we can save all of these, we might not 
 #use them all but they are all good to save anyway
 
-ggsave("derived_graphics/First.Base.Optim.Cluster.png", plot = p1)
-ggsave("derived_graphics/Second.Base.Optim.Cluster.png", plot= p2)
-ggsave("derived_graphics/Third.Base.Optim.Cluster.png", plot= p3)
+#now it's annoying, im only saving the ppt ones
+
+# ggsave("derived_graphics/First.Base.Optim.Cluster.png", plot = p1)
+# ggsave("derived_graphics/Second.Base.Optim.Cluster.png", plot= p2)
+# ggsave("derived_graphics/Third.Base.Optim.Cluster.png", plot= p3)
 ggsave("derived_graphics/Short.Stop.Optim.Cluster.png", plot= p4)
-ggsave("derived_graphics/Out.Field.Optim.Cluster.png", plot= p5)
-ggsave("derived_graphics/Catchers.Optim.Cluster.png", plot= p6)
+# ggsave("derived_graphics/Out.Field.Optim.Cluster.png", plot= p5)
+# ggsave("derived_graphics/Catchers.Optim.Cluster.png", plot= p6)
 
 
 #now actually make the plots
 
-p7 <- km.clust(B1)
-p8 <- km.clust(B2)
-p9 <- km.clust(B3)
+# p7 <- km.clust(B1)
+# p8 <- km.clust(B2)
+# p9 <- km.clust(B3)
 p10 <- km.clust(SS)
-p11 <- km.clust(OF)
-p12 <- km.clust(Catchers)
+# p11 <- km.clust(OF)
+# p12 <- km.clust(Catchers)
 
-#and save the plots
-ggsave("derived_graphics/First.Base.Cluster.png", plot = p7)
-ggsave("derived_graphics/Second.Base.Cluster.png", plot= p8)
-ggsave("derived_graphics/Third.Base.Cluster.png", plot= p9)
+#and save the plots, only ones in ppt rn
+# ggsave("derived_graphics/First.Base.Cluster.png", plot = p7)
+# ggsave("derived_graphics/Second.Base.Cluster.png", plot= p8)
+# ggsave("derived_graphics/Third.Base.Cluster.png", plot= p9)
 ggsave("derived_graphics/Short.Stop.Cluster.png", plot= p10)
-ggsave("derived_graphics/Out.Field.Cluster.png", plot= p11)
-ggsave("derived_graphics/Catchers.Cluster.png", plot= p12)
+# ggsave("derived_graphics/Out.Field.Cluster.png", plot= p11)
+# ggsave("derived_graphics/Catchers.Cluster.png", plot= p12)
 
 
 #now we need to do pitchers separately 
@@ -117,12 +119,12 @@ Pitch.cent.temp <- data.frame(
 Pitch.cent <- na.omit(cbind(Pitch.cent.temp, 
                             scale(Pitchers %>% 
                                     select(-c(AllStar, playerID, yearID, 
-                                              stint, teamID, lgID)), scale = T, center = T)))
+                                              teamID, lgID)), scale = T, center = T)))
 
 
 #ok so we're hoping for two clusters, lets check to see
 
-p13 <- fviz_nbclust(Pitch.cent[, -1], kmeans, method = "wss")
+# p13 <- fviz_nbclust(Pitch.cent[, -1], kmeans, method = "wss")
 
 #that is great we get a nice kink at 2
 
@@ -140,9 +142,9 @@ p14 <- fviz_cluster(k.pitch, data = Pitch.cent[, -1], geom = "point",
   guides(shape = F) +
   guides(col = F) +
   theme_minimal() +
-  geom_label(aes(x = 4.5, y = 6, label = "Not All-Stars"), col = "red") +
-  geom_label(aes(x = 4.5, y = 5, label = "All-Stars"), col = "blue") +
+  geom_label(aes(x = -4.5, y = 6, label = "Not All-Stars"), col = "red") +
+  geom_label(aes(x = -4.5, y = 5, label = "All-Stars"), col = "blue") +
   labs(title = "K-Means Clustering with Pitchers")
 
-ggsave("derived_graphics/Pitchers.Optim.Cluster.png", plot= p13)
+# ggsave("derived_graphics/Pitchers.Optim.Cluster.png", plot= p13)
 ggsave("derived_graphics/Pitchers.Cluster.png", plot= p14)
